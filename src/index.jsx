18cb93a2cbaefd2ac2b0c34e3b5e39d0c5f93538
shruti1608 <96721333+shruti1608@users.noreply.github.com>
2022-09-19 02:13:19 +0530

    greeting added

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App user="Enter Username" password="Enter Password"/>
+    <App user='Enter Username' password='Enter Password' style={{"fontSize":100}} />
   </React.StrictMode>,
  // React.createElement('h1',null,'hello!'),
   
