dba46095debd7c6e8f1b4da4bcec8702c310e0dc
shruti1608 <96721333+shruti1608@users.noreply.github.com>
2022-09-19 00:52:57 +0530

    counter

---
+++ src/counter.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function Counter(){
+    const [count,setcount] = useState(0)
+    function decrementcount(){
+        if(count  > 0){
+         setcount(count - 1)
+        }
+       }
+    function incrementcount(){  
+         setcount(count + 1)  
+        }
+    const rootStyle={
+        display:"flex",
+        flexDirection:"row"
+    }  
+    const buttonStyle={
+        margin:10,
+        padding:10  
+    }  
+    return(
+        <div style={rootStyle}>
+            <button style={buttonStyle} value={count} onClick={incrementcount}>+</button>
+            <h1>{count}</h1> 
+            <button style={buttonStyle} value={count} onClick={decrementcount}>-</button>
+
+        </div>
+    );
+}
