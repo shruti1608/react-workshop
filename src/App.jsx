53223848b5569a80b8ba6bc8999874b49481571a
shruti1608 <96721333+shruti1608@users.noreply.github.com>
2022-09-20 17:37:04 +0530

    solving prop-drilling issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 
-import Counter from './counter'
 
+//import Counter from './counter.jsx';
+import { useState } from 'react';
+import Header from './header.jsx';
+import Body from './body.jsx';
 
 export default function App(props) {
- 
-  return (
 
-<div className='App'>
-<Counter/>
-</div>
- 
+const [count,setcount] = useState(0)
+  return (
+    <div>
+     <Header count={count} setcount={setcount} />
+     <Body count={count} setcount={setcount}/>
+    </div>
+
   );
 }  
   
