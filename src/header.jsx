077c5e981df47d9bffcfdd1d0c3d73003984d4c5
shruti1608 <96721333+shruti1608@users.noreply.github.com>
2022-09-25 00:46:28 +0530

    multiple usecontext

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+import Counter from "./counter";
+
+export default function Header(){
+   // console.log(children[0])
+    return(
+        <div style={{"backgroundColor":"yellow"}}>
+            {/* {children[1]} */}
+         <Counter/>
+         
+        </div>
+    )
+}
