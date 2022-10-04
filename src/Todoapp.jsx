0bba6e7f70210e069e9ef0b528cbb9e922e6237a
shruti1608 <shruti.shah@mlveda.com>
2022-10-04 14:56:59 +0530

    css changes

--- src/Todoapp.jsx
+++ src/Todoapp.jsx
@@ -1,17 +1,20 @@
+
 import { useState } from "react";
-import Todolist from './Todolist'
 import { AiTwotoneEdit,AiFillDelete } from "react-icons/ai";
 import { BiSave } from "react-icons/bi";
 
+import './Todostyle.css';
+
 export default function Todoapp(){
+
 const [state,setstate] =useState('')
 const [list,setlist] = useState([])
-const [editstate,seteditstate] = useState(false)
-const [updateinput,setupdateinput] = useState(state)
+const [isedit,setisedit] = useState(false)
+//const [updateinput,setupdateinput] = useState(state)
 
 function onclickhandler(){
     
-    if(state != ''){
+    if(state !== ''){
     setlist(curr => [...curr,state])
     setstate('') 
     } 
@@ -22,84 +25,76 @@ function handledelete(id){
 // console.log(id,"delete")
  setlist(list.filter((item,idx) => idx !== id ))
 }
-function updateitm(id){
-   
-    seteditstate(true)
-    setlist([...list])
-   
-}
 
 
-const rootStyle={
-    justifyContent:"center",
-    flexDirection:"column",
-    display:"flex",
+function updateitem(itm,id,editstate){
+  console.log("inside updateitm",itm,id,editstate) 
+ // setisedit(true)
+
+  list.map((ele,idx) => { 
+    if(idx === id){
+        console.log(idx,id)
+        setisedit(true)
+    console.log("match",editstate)  
+    }
+   console.log(editstate,idx,id)
+})
+ //setlist([...list])
 }
-const textStyle={
-    alignSelf:"center",
-    color:"orange"
+
+function oncomplelehandler(id){
+    //save
+    console.log('inside oncompletehandler');
+    console.log(id,"save id")
+    setisedit(false)
 }
-const innerrootStyle={
-    marginTop:20,
-    alignSelf:"center",
-    flexDirection:"column"
+
+function onupdatehandler(e,id){
+  console.log('inside onupdatehandler')
+  console.log(e.target.value,id,"this")
+  setlist(list.map((item,idx) => {
+    if(idx === id){
+      console.log('put existing value')
+    } 
+    return item
+}))
+  setisedit(false)
 }
-const inputStyle={
-    boxShadow:"2px 2px orange",
-    padding:10,
-    width:500,
-    borderRadius:10
-}
-const buttonStyle={
-    marginLeft:10,
-    backgroundColor:"orange",
-    borderRadius:5,
-    height:30,
-    width:50,
-    boxShadow:"2px 2px 2px lightgray",
-    border:"1px solid orange"
-}
-const listStyle={
-    padding:10,
-    backgroundColor:"orange",
-    boxShadow:"2px 2px 2px lightgray",
-    marginTop:10,
-    borderRadius:10
-}
-const listtextStyle={
-    paddingLeft:10,
-}
+
 
 
     return(
 
-    <div style={rootStyle}>
-        <h1 style={textStyle}>MY Todos</h1>
-         <div style={innerrootStyle}>
+    <div className="rootStyle" >
+       
+        <h1 className="textStyle" >MY Todos</h1>
+         <div className="innerrootStyle" >
             
-            <input style={inputStyle}
+            <input  className="inputStyle" 
                value={state} 
                placeholder='Enter Task'
                onChange={(state)=> setstate(state.target.value)}/>
-            <button style={buttonStyle}
+            <button className="buttonStyle" 
                     value={list}
                     onClick={onclickhandler}>Add</button>
        
        
-            {list.map((itm,id) => <div key={id} style={listStyle}>
-                                 <div style={{display:"flex",justifyContent:"space-between"}}> 
-                                 {editstate ? 
-                                 <div style={{display:"flex",justifyContent:"space-between"}}>
-                                 <input value={updateinput} 
+            {list.map((itm,id) => <div key={id} className="listStyle">
+                                 <div className="innerlistStyle" > 
+                                 
+                                 {isedit  ? 
+                                 <div className="innerlistStyle">
+                                 <input defaultValue={state}
                                         style={{width:450}}
-                                        onChange={(e) => {setupdateinput(e.target.value);console.log(e.target.value,id,"this")}}></input> 
-                                 <BiSave style={{marginLeft:10}} onClick={() => {console.log(id,"save id");seteditstate(false)}}></BiSave>  
+                                        onChange={(e) => onupdatehandler(e,id)}></input> 
+                                 <BiSave style={{marginLeft:10}} 
+                                         onClick={() => oncomplelehandler(id)}></BiSave>  
                                  </div>     
                                  :
-                                  <span style={listtextStyle}>{itm}</span>
+                                  <span className="listtextStyle">{itm}</span>
                                   }
                                   <div>
-                                  <AiTwotoneEdit value={editstate} style={{marginRight:"10"}} onClick={() => updateitm(id)}></AiTwotoneEdit>
+                                  <AiTwotoneEdit onClick={() => updateitem(itm,id,isedit)} style={{marginRight:"10"}}></AiTwotoneEdit>
                                   <AiFillDelete  onClick={() => handledelete(id)}></AiFillDelete>
                                   </div>
                                   </div> 
---
+++ src/Todolist.jsx
@@ -0,0 +1,7 @@
+export default function Todolist(){
+    return(
+        <div className="rootStyle">
+          <span className="textStyle">todo app</span>
+        </div>
+    )
+}
