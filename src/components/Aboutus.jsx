a1b8c1e38d3196f34eb998e6d386441c94ed7779
shruti1608 <shruti.shah@mlveda.com>
2022-10-28 19:06:44 +0530

    formatting

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Aboutus(){
     return(
         <div>
             <span>About us</span>
+            
         </div>
     )
 }
